aabfe57b049b004082a09f3121f27733bc7445d3
joemulti <bjoern.engel@experteach.de>
2020-03-24 12:14:26 +0100

    Env

--- SimpleGetRooms.py
+++ SimpleGetRooms.py
@@ -1,15 +1,16 @@
 # Dieses Beispiel holt sich mittels eines Get Befehls die Räume des Nutzers mit dem angegebenen Access Token
-
+from dotenv import load_dotenv
+load_dotenv()
 # Das Script arbeitet mit der Library "requests". Gegebenenfalls muss diese mit "pip install requests" nachinstalliert werden.
 import requests
-import 
+import os
+
 
 # Gemäss der API Dokumentation sind alle Aktionen bezüglich Räumen unter folgender URL abzurufen.
 apiUrl = "https://api.ciscospark.com/v1/rooms"
 
 # Das Access Token findet man unter "developer.webex.com"
-access_token = access_token
-
+access_token = os.getenv("AccessToken")
 # 
 httpHeaders = {"Content-type" : "application/json", "Authorization" : "Bearer " + access_token}
 
--- SimplePostMessage.py
+++ SimplePostMessage.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
 import requests
-import 
+import os 
 
 # Das Script arbeitet mit der Library "requests". Gegebenenfalls muss diese mit "pip install requests" nachinstalliert werden.
 apiUrl = "https://api.ciscospark.com/v1/messages"
-access_token = acess
+access_token = os.getenv("AccessToken") 
 httpHeaders = {"Content-type" : "application/json", "Authorization" : "Bearer " + access_token}
 
 body = {"toPersonEmail" : "gifbot@webex.bot", "text" : "Hello"}
